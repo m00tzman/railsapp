f2a867d22ea3b3135d76e88f1c98d069b6a728ed
Cory Rabiea <m00tzmanAlmighty@gmail.com>
2015-04-09 20:48:35 -0400

    removed all binding prys

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
 	def index
-		binding.pry
 		@students = Student.all
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-binding.pry
 resources :students, only: [:index]
 root 'home#show'
 end
